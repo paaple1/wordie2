82ddd707696a2ed6bda337211fd86cbb4b09be1c
paaple <tc-52@outlook.jp>
2024-08-11 01:22:43 +0900

    Change index.js file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require('cors');
+app.use(cors());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/search', async (req, res) => {
@@ -18,12 +22,16 @@ app.get('/search', async (req, res) => {
                 'X-RapidAPI-Host': 'lexicala1.p.rapidapi.com'
             }
         });
+        console.log(response.data);
         res.json(response.data);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: 'Error fetching data from Lexicala API' });
     }
 });
 
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
